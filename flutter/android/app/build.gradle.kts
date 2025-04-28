a7978df5cf680a7e833790e227207c18adeb7add
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-04-28 14:35:51 +0300

    update plugins

--- flutter/android/app/build.gradle.kts
+++ flutter/android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
         applicationId = "com.example.pax"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        ndkVersion = "27.0.12077973"
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
