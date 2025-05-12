cee61b588c0e96d94715a5cbedf83b39a8de1391
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-05-12 20:01:33 +0300

    add fcm setup

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pax/firebase_options.dart';
+import 'package:pax/repositories/fcm/fcm_provider.dart';
 import 'package:pax/routing/service.dart';
 import 'package:pax/widgets/app_lifecycle_handler.dart';
 import 'theming/theme_provider.dart';
@@ -9,6 +11,7 @@ import 'package:shadcn_flutter/shadcn_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await _initializeFirebaseMessaging();
   runApp(ProviderScope(child: AppLifecycleHandler(child: App())));
 }
 
@@ -22,9 +25,11 @@ class App extends ConsumerStatefulWidget {
 class _AppState extends ConsumerState<App> {
   @override
   Widget build(BuildContext context) {
+    final router = ref.watch(routerProvider);
+    ref.watch(fcmInitProvider);
     return ShadcnApp.router(
       debugShowCheckedModeBanner: false,
-      routerConfig: routerConfig,
+      routerConfig: router,
       title: 'My App',
       theme: ref.watch(themeProvider),
       builder: (context, child) {
@@ -37,4 +42,14 @@ class _AppState extends ConsumerState<App> {
       },
     );
   }
+}
+
+// Initialize Firebase Messaging
+Future<void> _initializeFirebaseMessaging() async {
+  // Set up foreground notification presentation options
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
 }
