d958978d9f68568c358bbefce61c6006744a21f8
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-05-18 22:46:39 +0300

    add shared config variables

--- flutter/functions/shared/config.ts
+++ flutter/functions/shared/config.ts
@@ -1,6 +1,10 @@
-import { Address } from 'viem';
+import { Address, createPublicClient, http } from 'viem';
 import { CallableOptions } from 'firebase-functions/v2/https';
 import { config } from 'dotenv';
+import { celo } from 'viem/chains';
+import { createPimlicoClient } from 'permissionless/clients/pimlico';
+import { entryPoint07Address } from 'viem/account-abstraction';
+import { PrivyClient } from '@privy-io/server-auth';
 
 
 config();
@@ -23,3 +27,25 @@ export const CREATE2_FACTORY = "0x4e59b44847b379578588920cA78FbF26c0B4956C" as A
 
 // API endpoint configs
 export const PIMLICO_URL = `https://api.pimlico.io/v2/42220/rpc?apikey=${PIMLICO_API_KEY}`;
+
+
+export const PUBLIC_CLIENT = createPublicClient({
+  chain: celo,
+  transport: http(),
+});
+
+
+
+export const PIMLICO_CLIENT = createPimlicoClient({
+  transport: http(PIMLICO_URL),
+  entryPoint: {
+    address: entryPoint07Address,
+    version: "0.7",
+  },
+});
+
+export const PRIVY_CLIENT = new PrivyClient(PRIVY_APP_ID, PRIVY_APP_SECRET, {
+  walletApi: {
+    authorizationPrivateKey: PRIVY_WALLET_AUTH_PRIVATE_KEY,
+  },
+});
