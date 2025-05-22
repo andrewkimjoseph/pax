0561009be877691e84ca0563f3ca45a13797872f
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-05-22 15:27:29 +0300

    chore: update Android package name and MainActivity

--- flutter/android/app/build.gradle.kts
+++ flutter/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "xyz.thecanvassing.pax"
+    namespace = "app.thepax.android"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "xyz.thecanvassing.pax"
+        applicationId = "app.thepax.android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         ndkVersion = "27.0.12077973"
---
+++ flutter/android/app/src/main/kotlin/app/thepax/android/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.thepax.android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
