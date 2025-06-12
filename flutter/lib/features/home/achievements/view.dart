5bbc8a448ec40f5ee04d4d3a50bca3dbf040ab5b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-06-12 19:13:22 +0300

    chore: update achievements view and pubspec.yaml

--- flutter/lib/features/home/achievements/view.dart
+++ flutter/lib/features/home/achievements/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pax/providers/auth/auth_provider.dart';
 import 'package:pax/providers/db/achievement/achievement_provider.dart';
 import 'package:pax/providers/db/participant/participant_provider.dart';
 import 'package:pax/theming/colors.dart';
@@ -19,15 +20,14 @@ class AchievementsView extends ConsumerStatefulWidget {
 class _AchievementsViewState extends ConsumerState<AchievementsView> {
   int index = 0;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // Load achievements when the page is initialized
-  //   WidgetsBinding.instance.addPostFrameCallback((_) {
-  //     final participantId = ref.read(authProvider).user.uid;
-  //     ref.read(achievementProvider.notifier).fetchAchievements(participantId);
-  //   });
-  // }
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final participantId = ref.read(authProvider).user.uid;
+      ref.read(achievementProvider.notifier).fetchAchievements(participantId);
+    });
+  }
 
   bool _isProfileComplete(Participant? participant) {
     if (participant == null) return false;
