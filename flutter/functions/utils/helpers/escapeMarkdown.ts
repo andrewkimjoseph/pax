c54291ceb3a68172bd48b4c650ba280786819623
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-07-12 14:59:53 +0300

    fix: improve URL and display name formatting in Telegram notifications

--- flutter/functions/utils/helpers/escapeMarkdown.ts
+++ flutter/functions/utils/helpers/escapeMarkdown.ts
@@ -9,10 +9,12 @@ export function escapeMarkdown(text: string): string {
   // If the text looks like a URL (starts with http/https), handle it specially
   if (text.startsWith('http://') || text.startsWith('https://')) {
     // For URLs, only escape characters that are not part of valid URL format
-    return text.replace(/([_*\[\]()~`>#+={}!])/g, '\\$1');
+    // Don't escape equals signs as they're used for query parameters
+    return text.replace(/([_*\[\]()~`>#+{}!])/g, '\\$1');
   }
   
-  // For regular text, escape all special characters including dots
-  return text.replace(/([_*\[\]()~`>#+\-=|{}.!])/g, '\\$1');
+  // For regular text, escape only characters that actually affect Markdown formatting
+  // Don't escape parentheses as they're safe in regular text
+  return text.replace(/([_*\[\]~`>#+\-=|{}.!])/g, '\\$1');
 }
   
