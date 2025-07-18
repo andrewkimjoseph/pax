1064960cbd8b54b4366c5049e6150edc964239da
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-07-18 09:23:13 +0300

    refactor(config): use PAX_MASTER_PRIVATE_KEY and derived account
    - Remove PAX_MASTER_SMART_ACCOUNT and PAX_MASTER_EOA
    - Add PAX_MASTER_PRIVATE_KEY and PAX_MASTER_PRIVATE_KEY_ACCOUNT for improved key management

--- flutter/functions/utils/config.ts
+++ flutter/functions/utils/config.ts
@@ -4,6 +4,7 @@ import { config } from 'dotenv';
 import { celo } from 'viem/chains';
 import { PrivyClient } from '@privy-io/server-auth';
 import * as admin from "firebase-admin";
+import { privateKeyToAccount } from 'viem/accounts';
 
 config();
 
@@ -12,8 +13,7 @@ export const PRIVY_APP_SECRET = process.env.PRIVY_APP_SECRET || '';
 export const PRIVY_WALLET_AUTH_PRIVATE_KEY = process.env.PRIVY_WALLET_AUTH_PRIVATE_KEY || '';
 export const PIMLICO_API_KEY = process.env.PIMLICO_API_KEY || '';
 export const DRPC_API_KEY = process.env.DRPC_API_KEY || '';
-export const PAX_MASTER_SMART_ACCOUNT = `0x${process.env.PAX_MASTER_SMART_ACCOUNT}` as Address;
-export const PAX_MASTER_EOA = `0x${process.env.PAX_MASTER_EOA}` as Address;
+export const PAX_MASTER_PRIVATE_KEY = process.env.PAX_MASTER_PRIVATE_KEY as Address;
 
 export const PAXACCOUNT_IMPLEMENTATION_ADDRESS = process.env.PAXACCOUNT_IMPLEMENTATION_ADDRESS as Address;
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '';
@@ -47,6 +47,8 @@ export const PRIVY_CLIENT = new PrivyClient(PRIVY_APP_ID, PRIVY_APP_SECRET, {
     authorizationPrivateKey: PRIVY_WALLET_AUTH_PRIVATE_KEY,
   },
 });
+
+export const PAX_MASTER_PRIVATE_KEY_ACCOUNT = privateKeyToAccount(PAX_MASTER_PRIVATE_KEY);
 
 export const DB = admin.firestore;
 
