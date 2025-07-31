37af8b690553965fb8e3819257cadd686ef13b52
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-07-31 09:35:49 +0300

    Update FAQ content: add researcher dependency note and timezone variations for faucet refill

--- flutter/lib/data/faqs.dart
+++ flutter/lib/data/faqs.dart
@@ -4,7 +4,8 @@ class FAQs {
   static const List<FAQ> faqs = [
     FAQ(
       question: "How many times does the platform run micro tasks?",
-      answer: "Once 1️⃣ every week. So, a total of 4 tasks a month.",
+      answer:
+          "Once 1️⃣ every week. So, a total of 4 tasks a month. However, this is dependent on the availability of researchers.",
     ),
     FAQ(
       question: "What time of the day is the micro task made available?",
@@ -12,7 +13,7 @@ class FAQs {
     ),
     FAQ(
       question: "On which day is the micro task available?",
-      answer: "Wednesday - Wednesday of every week is PaxDay 🥳",
+      answer: "Tuesday - Tuesday is PaxDay 🥳",
     ),
     FAQ(
       question: "What does GoodDollar Face Verification Required mean?",
@@ -25,7 +26,6 @@ class FAQs {
       answer:
           "Tasks are available on a first come, first served basis. Because of the high volumes, once a task is listed, people rush and thus, the task closes faster. You just need to be quick enough.",
     ),
-
     FAQ(
       question: "How do I convert my G\$ tokens to cUSD from within Minipay",
       answer:
@@ -34,7 +34,7 @@ class FAQs {
     FAQ(
       question: "What type of micro tasks are available?",
       answer:
-          "At the moment, we have only one type of micro task: form filling. We will be adding more types of tasks in the future.",
+          "At the moment, we have only one type of micro tasks: surveys. We will be adding more types of micro tasks in the future.",
     ),
     FAQ(
       question: "How many questions are there for a task?",
@@ -50,6 +50,12 @@ class FAQs {
           "I keep getting a message that I have been banned from the platform. What does this mean?",
       answer:
           "This means that your account has been disabled. You will not be able to complete any tasks and you will not be able to withdraw your funds. Contact support if you think this is a mistake.",
+    ),
+    FAQ(
+      question:
+          "When I try to claim achievements, I get an error: 'Exception: B: Claiming is not possible at this time'. What does this mean?",
+      answer:
+          "This means that the faucet has not been refilled yet. You need to wait for the faucet to be refilled before you can claim an achievement, and this happens daily between 8AM UTC / 9AM WAT / 10AM CAT / 11AM EAT and 3PM UTC / 4PM WAT / 5PM CAT / 6PM EAT.",
     ),
   ];
 }
