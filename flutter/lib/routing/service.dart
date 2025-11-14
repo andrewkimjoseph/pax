87e9de2d4a7317cae0fe6f8658e8d49478e7d812
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-11-14 11:23:20 +0300

    refactor(routing): remove canvassingXGoodDollar route
    - Comment out canvassingXGoodDollar route in router
    - Remove import for CanvassingXGoodDollarView
    - Delete canvassing_x_gooddollar widget
    - Update canvassing_x_gooddollar view (functionality moved to drawer)

--- flutter/lib/routing/service.dart
+++ flutter/lib/routing/service.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pax/exports/views.dart';
 import 'package:pax/features/account_and_security/view.dart';
-import 'package:pax/features/canvassing_x_gooddollar/view.dart';
+// import 'package:pax/features/canvassing_x_gooddollar/view.dart';
 import 'package:pax/features/claim_reward/view.dart';
 import 'package:pax/features/report_page/view.dart';
 import 'package:pax/features/task/task_itself/check_out_app/view.dart';
@@ -101,10 +101,10 @@ final routerProvider = Provider((ref) {
         path: Routes.onboarding,
         builder: (context, state) => const OnboardingView(),
       ),
-      GoRoute(
-        path: Routes.canvassingXGoodDollar,
-        builder: (context, state) => const CanvassingXGoodDollarView(),
-      ),
+      // GoRoute(
+      //   path: Routes.canvassingXGoodDollar,
+      //   builder: (context, state) => const CanvassingXGoodDollarView(),
+      // ),
       GoRoute(
         path: Routes.activity,
         builder: (context, state) => const ActivityView(),
